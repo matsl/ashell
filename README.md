aead254b7f64ef62d28215174d7ea4c94abdfacf
Mats Lidell <mats.lidell@lidells.se>
2025-10-25 16:51:02 +0200

    Add a derived mode for binding keys in ashell-mode

--- README.md
+++ README.md
@@ -9,21 +9,31 @@ To not overflow Emacs with possibly uninteresting buffers there is a
 configurable cap on how many buffers are kept,
 `ashell-output-buffer-history-size`. The youngest buffers are kept.
 Buffers with active processes or buffers set to read only state are
-not subject for removal.
+also kept and are not subject for removal.
 
 To view the history of commands or act on the buffers there is an
 adoption of ibuffer, `ashell-ibuffer`.
 
-## Example key bindings
+## Key bindings
+
+AShell mode sets the following keys.
+
+```el
+(define-key ashell-command-mode-map (kbd "C-c C-r") #'ashell-rerun)
+(define-key ashell-command-mode-map (kbd "C-c C-p") #'ashell-buffer-prev)
+(define-key ashell-command-mode-map (kbd "C-c C-n") #'ashell-buffer-next)
+(define-key ashell-command-mode-map (kbd "C-c C-b") #'ashell-ibuffer)
+```
+
+The following key binding are suggestions for global bindings you
+might want to use or you can choose something else.
 
 ```el
 (define-key esc-map "&" 'ashell-command)
-(global-set-key "\C-c&" 'ashell-ibuffer)
-(define-key shell-command-mode-map "\C-cr" 'ashell-rerun)
+(global-set-key "\C-c\M-&" 'ashell-ibuffer)
 ```
 
 ## Hyperbole support
 
 Load hib-ashell.el to replace Hyperboles action types `exec-shell-cmd`
 and `exec-window-cmd`.
-
--- ashell.el
+++ ashell.el
@@ -1,6 +1,6 @@
 ;;; ashell.el --- background shell command with history  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2017-2024 Free Software Foundation, Inc.
+;; Copyright (C) 2017-2025 Free Software Foundation, Inc.
 
 ;; Author: Mats Lidell <matsl@gnu.org>
 ;; Keywords: tools
@@ -26,6 +26,7 @@
 
 (require 'ibuffer)
 (require 'dired)
+(require 'shell)
 
 (defcustom ashell-output-buffer-history-size 20
   "Number of old shell buffers to keep."
@@ -37,10 +38,66 @@
 (defvar ashell-command ""
   "Store a buffer local value of the command executed for display purposes.")
 
+ (define-derived-mode ashell-command-mode shell-command-mode "MyShell"
+   "Major mode for ashell-command buffers."
+   ;; Keys
+   (define-key ashell-command-mode-map (kbd "C-c C-r") #'ashell-rerun)
+   (define-key ashell-command-mode-map (kbd "C-c C-p") #'ashell-buffer-prev)
+   (define-key ashell-command-mode-map (kbd "C-c C-n") #'ashell-buffer-next)
+   (define-key ashell-command-mode-map (kbd "C-c C-b") #'ashell-ibuffer)
+
+   ;; Mode line-indikator
+   (setq mode-name "AShell"))
+
 (defun ashell-output-buffer-name-generator ()
   "Generate a unique buffer name."
   (setq ashell-output-buffer-name-number (1+ ashell-output-buffer-name-number))
   (concat ashell-output-buffer-name-prefix "<" (number-to-string ashell-output-buffer-name-number) ">*"))
+
+(defun ashell--buffer-list ()
+  "Return all ashell buffers as a sorted list."
+  (let* ((buffers (buffer-list))
+         (buffer-name-regexp
+          (concat
+           (replace-regexp-in-string "\\*" "\\\\*" ashell-output-buffer-name-prefix)
+           "<[[:digit:]]+>\\\\*"))
+	 (shell-buffers
+	  (sort
+	   (delq nil
+		 (mapcar
+		  (lambda (x) (and (string-match buffer-name-regexp
+						 (buffer-name x))
+				   (buffer-name x)))
+		  buffers))
+	   (lambda (s1 s2) (string-version-lessp s1 s2)))))
+    shell-buffers))
+
+(defun ashell--next-in-list-or-first (item list)
+  "Return element after ITEM in LIST.
+If on the last ITEM then wrap around and return the first element."
+  (let ((rest (member item list)))
+    (if (cdr rest)
+        (cadr rest)
+      (car list))))
+
+(defun ashell--prev-in-list-or-last (item list)
+  "Return element before ITEM in LIST.
+If on the first if ITEM then wrap around and return the last element."
+  (ashell--next-in-list-or-first item (reverse list)))
+
+(defun ashell-buffer-next ()
+  "Goto to next ashell buffer."
+  (interactive)
+  (let ((shell-buffers (ashell--buffer-list))
+        (bn (buffer-name)))
+    (switch-to-buffer (ashell--next-in-list-or-first bn shell-buffers))))
+
+(defun ashell-buffer-prev ()
+  "Goto to next ashell buffer."
+  (interactive)
+  (let ((shell-buffers (ashell--buffer-list))
+        (bn (buffer-name)))
+    (switch-to-buffer (ashell--prev-in-list-or-last bn shell-buffers))))
 
 (defun ashell-garbage-collect-old-buffers ()
   "Limit number of ashell buffers by removing old buffers.
@@ -68,11 +125,12 @@ kept."
     (dolist (buff (butlast shell-buffers (1- ashell-output-buffer-history-size)))
       (kill-buffer buff))))
 
-(defun ashell-command (command &optional hide-command hide-window)
+(defun ashell-command (command &optional hide-command hide-window pop-to-shell)
   "Run asynchronous shell COMMAND in a new buffer.
 Set HIDE-COMMAND to t for not showing the command in the output buffer.
 With optional HIDE-WINDOW do not display the shell output buffer.  Keep
-`ashell-output-buffer-history-size' buffers as history."
+`ashell-output-buffer-history-size' buffers as history.
+With non-nil POP-TO-SHELL make the shell buffer current."
   (interactive
    (list
     (read-shell-command (concat ashell-output-buffer-name-prefix ": ") nil nil
@@ -84,19 +142,23 @@ With optional HIDE-WINDOW do not display the shell output buffer.  Keep
 			  (and filename (file-relative-name filename))))))
   (let ((buffer (get-buffer-create (ashell-output-buffer-name-generator)))
 	(shell-command-dont-erase-buffer t)
-        (async-shell-command-display-buffer (not hide-window)))
+        (async-shell-command-display-buffer (not hide-window))
+        (async-shell-command-mode 'ashell-command-mode))
     (when (not hide-command)
       (with-current-buffer buffer
         (insert "$ " command "\n")))
     (async-shell-command command buffer nil)
     (with-current-buffer buffer
       (setq-local ashell-command command))
+    (when pop-to-shell
+      (pop-to-buffer buffer))
     (ashell-garbage-collect-old-buffers)))
 
-(defun ashell-rerun ()
-  "Rerun the command."
-  (interactive)
-  (ashell-command ashell-command))
+(defun ashell-rerun (pop-to-shell)
+  "Rerun the command.
+With prefix POP-TO-SHELL make the new shell current."
+  (interactive "P")
+  (ashell-command ashell-command nil nil pop-to-shell))
 
 (define-ibuffer-column ashell-cmd (:name "Cmd")
   "The command executed in the async shell."
